4395816f23f373abffa51a54b6f7ada3fec222fa
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2024-07-25 15:53:20 -0300

    Add exercicio-trycatch.js

---
+++ src/exercicio-trycatch.js
@@ -0,0 +1,45 @@
+class DivisaoPorZeroError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'DivisaoPorZeroError';
+    }
+}
+
+class ResultadoMenorQueUmError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'ResultadoMenorQueUmError';
+    }
+}
+
+let carregando = false;
+
+try {
+    carregando = true;
+    let a = 10;
+    let b = 2;
+
+    if (b === 0) {
+        throw new DivisaoPorZeroError('ERRO DE DIVISÃO POR ZERO!');
+    }
+
+    let result = a / b;
+
+    if (result < 1) {
+        throw new ResultadoMenorQueUmError('O RESULTADO DA DIVISÃO NÃO PODE SER MENOR QUE 1!');
+    }
+
+    console.log('CALCULOU!');
+    console.log('RESULTADO:', result);
+} catch (error) {
+    if (error instanceof DivisaoPorZeroError || error instanceof ResultadoMenorQueUmError) {
+        console.log('ERRO!');
+        console.log(error.message);
+    } else {
+        console.log('ERRO INESPERADO!');
+        console.log(error.message);
+    }
+} finally {
+    carregando = false;
+    console.log('SEMPRE EXECUTADO!');
+}
